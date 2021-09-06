5e902e4963327f30d75a8c0b41a5c0c24ae6f196
Woosang <yws1502@naver.com>
2021-09-07 01:22:57 +0900

    Setting Static, Media file

--- config/settings.py
+++ config/settings.py
@@ -135,6 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
