a6e4cf663ad12c258ce8779f8372dc89dd73cd74
Woosang <yws1502@naver.com>
2021-09-10 02:32:27 +0900

    create Message table in users app

--- users/models.py
+++ users/models.py
@@ -60,3 +60,17 @@ class Algorithm(models.Model):
 
     def __str__(self):
         return self.name
+
+class Message(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True)
+    recipient = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, related_name='messages')
+    name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(max_length=200, null=True, blank=True)
+    subject = models.CharField(max_length=200)
+    body = models.TextField(max_length=500, null=True, blank=True)
+    is_read = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid4, primary_key=True, unique=True, editable=False)
+
+    def __str__(self):
+        return self.name + self.subject
